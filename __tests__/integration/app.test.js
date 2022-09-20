5bfd2cfde189084cd52031adb0f5ea9887c2c951
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-09-20 12:56:25 +0100

    test coverage

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -2,8 +2,8 @@ const http = require("http");
 const supertest = require("supertest");
 const app = require("../../app");
 
-describe("Test the root path", () => {
-	let server;
+describe("Test the CRUD actions", () => {
+  let server;
 
 	beforeAll((done) => {
 		server = http.createServer(app.requestHandler);
@@ -14,22 +14,25 @@ describe("Test the root path", () => {
 		server.close(done);
 	});
 
-	it("should response the GET method", async () => {
+  it("GET => should return an array of objects", async () => {
 		const response = await supertest(server).get("/books");
 
     expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.text).length).toBeGreaterThan(0);
 	});
 
-  // it("should create a book in the directory", async () => {
-	// 	const response = await supertest(server).post("/books").send({
-  //     "title": "A Book",
-  //     "author": "An Author",
-  //     "year": 2022
-	// 	});
+  it("POST => should create a book in the directory with an auto-generated id", async () => {
+		const response = await supertest(server).post("/books").send({
+      "title": "A Book",
+      "author": "An Author",
+      "year": 2022
+		});
 
-	// 	expect(response.status).toBe(200);
-	// 	expect(response.text).toBe(JSON.stringify({ title: "A Book",
-  //   author: "An Author",
-  //   year: 2022 }));
-	// });
+		expect(response.statusCode).toBe(201);
+    expect(JSON.parse(response.text)).toHaveProperty("title", "A Book");
+    expect(JSON.parse(response.text)).toHaveProperty("author", "An Author");
+    expect(JSON.parse(response.text)).toHaveProperty("year", 2022);
+    expect(JSON.parse(response.text)).toHaveProperty("id");
+    expect(JSON.parse(response.text).id).toBeTruthy;
+	});
 });
