4ad7971fce252b7b4a0856ab9bef7629969f9ad9
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-09-21 23:24:28 +0100

    refactoring for express: get all books

--- app.js
+++ app.js
@@ -1,92 +1,53 @@
-const http = require("http");
-const { getAllBooks, addBook, updateBook, deleteBook } = require("./src/books");
-const { createUser, getAllUsers, authenticate } = require("./src/users");
+const express = require("express");
+const bookRoute = require("./routes/book");
 
-function requestHandler(req, res) {
-	res.setHeader("Content-Type", "application/json");
-  //Configure CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+const app = express();
 
-	// Book routes
-	if (req.url === "/books" && req.method === "GET") {
-		// Get all books => GET
-		authenticate(req, res, ["admin", "reader"])
-			.then(() => {
-				getAllBooks(req, res);
-			})
-			.catch((err) => {
-				res.statusCode = 401;
-				res.end(
-					JSON.stringify({
-						error: err,
-					})
-				);
-			});
-	} else if (req.url === "/books" && req.method === "POST") {
-		// Add a book => POST
-		authenticate(req, res, ["admin"])
-			.then(() => {
-				addBook(req, res);
-			})
-			.catch((err) => {
-				res.statusCode = 401;
-				res.end(
-					JSON.stringify({
-						error: err,
-					})
-				);
-			});
-	} else if (req.url === "/books" && req.method === "PUT") {
-		// Update book => PUT
-    authenticate(req, res, ["admin"])
-			.then(() => {
-				updateBook(req, res);
-			})
-			.catch((err) => {
-				res.statusCode = 401;
-				res.end(
-					JSON.stringify({
-						error: err,
-					})
-				);
-			});
-	} else if (req.url === "/books" && req.method === "DELETE") {
-		// Delete book => DELETE
-		authenticate(req, res, ["admin"])
-			.then(() => {
-				deleteBook(req, res);
-			})
-			.catch((err) => {
-				res.statusCode = 401;
-				res.end(
-					JSON.stringify({
-						error: err,
-					})
-				);
-			});
-	} else if (req.url === "/users" && req.method === "GET") {
-		authenticate(req, res, ["admin"])
-			.then(() => {
-				getAllUsers(req, res);
-			})
-			.catch((err) => {
-				res.statusCode = 401;
-				res.end(
-					JSON.stringify({
-						error: err,
-					})
-				);
-			});
-	} else if (req.url === "/users/register" && req.method === "POST") {
-		createUser(req, res);
-	} else {
-		res.statusCode = 404;
-		res.end("Not Found");
-	}
-	// LoanOut => POST
-	// Return => POST
-}
+// express body parser
+app.use(express.json());
 
-const server = http.createServer(requestHandler);
+app.use("/books", bookRoute);
 
-module.exports = { server, requestHandler };
+app.get('/', (req, res) => {
+  res.end('Home Page');
+});
+
+module.exports = { app };
+
+
+
+// const http = require("http");
+// const { getAllBooks, addBook, updateBook, deleteBook } = require("./src/books");
+// const { createUser, getAllUsers, authenticate } = require("./src/users");
+
+// function requestHandler(req, res) {
+// 	res.setHeader("Content-Type", "application/json");
+//   //Configure CORS
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+// 	// user routes
+// 	if (req.url === "/users" && req.method === "GET") {
+// 		authenticate(req, res, ["admin"])
+// 			.then(() => {
+// 				getAllUsers(req, res);
+// 			})
+// 			.catch((err) => {
+// 				res.statusCode = 401;
+// 				res.end(
+// 					JSON.stringify({
+// 						error: err,
+// 					})
+// 				);
+// 			});
+// 	} else if (req.url === "/users/register" && req.method === "POST") {
+// 		createUser(req, res);
+// 	} else {
+// 		res.statusCode = 404;
+// 		res.end("Not Found");
+// 	}
+// 	// LoanOut => POST
+// 	// Return => POST
+// }
+
+// const server = http.createServer(requestHandler);
+
